7f95b122fba95c3c73f813860e2bb5490e407485
Your Name <you@example.com>
2025-12-09 12:46:52 +0530

    Adding a new file

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass
+{
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to java");	
+		System.out.println("we aRE LErning to how to create java file and execute/run the program");
+	}
+}

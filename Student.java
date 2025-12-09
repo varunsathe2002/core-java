5534c4a96724a6ac28da708f4c4979e812983901
Your Name <you@example.com>
2025-12-09 12:59:32 +0530

    student

---
+++ Student.java
@@ -0,0 +1,10 @@
+﻿class Student
+{
+	public static void main(String[] args)
+	{
+		System.out.println("my name is varun");
+		System.out.println("i'm studying MCA");
+		System.out.println("my college name is KLESNC");
+		System.out.println("2025 passedout");
+	}
+}

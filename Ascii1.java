9d1c797893c3e349d3a72e030eaeaf12a932b8f4
Your Name <you@example.com>
2025-12-10 11:43:05 +0530

    Ascii1.java

---
+++ Ascii1.java
@@ -0,0 +1,142 @@
+public class Ascii1 {
+    public static void main(String[] args){
+        char character='g';
+        if(character=='a'){
+            System.out.println("Ascii value of a is "+97);
+        }else if(character=='b'){
+            System.out.println("Ascii value of b is "+98);
+        }else if(character=='c'){
+            System.out.println("Ascii value of c is "+99);
+        }else if(character=='d'){
+            System.out.println("Ascii value of d is "+100);
+        }else if(character=='e'){
+            System.out.println("Ascii value of e is "+101);
+        }else if(character=='f'){
+            System.out.println("Ascii value of f is: "+102);
+        }else if(character=='g'){
+            System.out.println("Ascii value of g is: "+103);
+        }else if(character=='h'){
+            System.out.println("Ascii value of h is: "+104);
+        }else if(character=='i'){
+            System.out.println("Ascii value of i is: "+105);
+        }else if(character=='j'){
+            System.out.println("Ascii value of j is: "+106);
+        }else if(character=='k'){
+            System.out.println("Ascii value of k is: "+107);
+        }else if(character=='l'){
+            System.out.println("Ascii value of l is: "+108);
+        }else if(character=='m'){
+            System.out.println("Ascii value of m is: "+109);
+        }else if(character=='n'){
+            System.out.println("Ascii value of n is: "+110);
+        }else if(character=='o'){
+            System.out.println("Ascii value of o is: "+111);
+        }else if(character=='p'){
+            System.out.println("Ascii value of p is: "+112);
+        }else if(character=='q'){
+            System.out.println("Ascii value of q is: "+113);
+        }else if(character=='r'){
+            System.out.println("Ascii value of r is: "+114);
+        }else if(character=='s'){
+            System.out.println("Ascii value of s is: "+115);
+        }else if(character=='t'){
+            System.out.println("Ascii value of t is: "+116);
+        }else if(character=='u'){
+            System.out.println("Ascii value of u is: "+117);
+        }else if(character=='v'){
+            System.out.println("Ascii value of v is: "+118);
+        }else if(character=='w'){
+            System.out.println("Ascii value of w is: "+119);
+        }else if(character=='x'){
+            System.out.println("Ascii value of x is: "+120);
+        }else if(character=='y'){
+            System.out.println("Ascii value of y is: "+121);
+        }else if(character=='z'){
+            System.out.println("Ascii value of z is: "+122);
+        }
+        switch (character) {
+            case 'a':
+                System.out.println("Ascii value of a is: "+97);           
+                break;
+            case 'b':
+                System.out.println("Ascii value of b is: "+98);
+                break;
+            case 'c':
+                System.out.println("Ascii value of c is: "+99);
+                break;
+            case 'd':
+                System.out.println("Ascii value of d is: "+100);
+                break;
+            case 'e':
+                System.out.println("Ascii value of e is: "+101);
+                break;
+            case 'f':
+                System.out.println("Ascii value of f is: "+102);
+                break;
+            case 'g':
+                System.out.println("Ascii value of g is: "+103);
+                break;
+            case 'h':
+                System.out.println("Ascii value of h is: "+104);
+                break;
+            case 'i':
+                System.out.println("Ascii value of i is: "+105);
+                break;
+            case 'j':
+                System.out.println("Ascii value of j is: "+106);
+                break;
+            case 'k':
+                System.out.println("Ascii value of k is: "+107);
+                break;
+            case 'l':
+                System.out.println("Ascii value of l is: "+108);
+                break;
+            case 'm':
+                System.out.println("Ascii value of m is: "+109);
+                break;
+            case 'n':
+                System.out.println("Ascii value of n is: "+110);
+                break;
+            case 'o':
+                System.out.println("Ascii value of o is: "+111);
+                break;
+            case 'p':
+                System.out.println("Ascii value of p is: "+112);
+                break;
+            case 'q':
+                System.out.println("Ascii value of q is: "+113);
+                break;
+            case 'r':
+                System.out.println("Ascii value of r is: "+114);
+                break;
+            case 's':
+                System.out.println("Ascii value of s is: "+115);
+                break;
+            case 't':
+                System.out.println("Ascii value of t is: "+116);
+                break;
+            case 'u':
+                System.out.println("Ascii value of u is: "+117);
+                break;
+            case 'v':
+                System.out.println("Ascii value of v is: "+118);
+                break;
+            case 'w':
+                System.out.println("Ascii value of w is: "+119);
+                break;
+            case 'x':
+                System.out.println("Ascii value of x is: "+120);
+                break;
+            case 'y':
+                System.out.println("Ascii value of y is: "+121);
+                break;
+            case 'z':
+                System.out.println("Ascii value of z is: "+122);
+                break;
+            default:
+                System.out.println("Invaild Number");
+                break;
+        }
+
+    }
+}

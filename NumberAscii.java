8b714a3fced2f3b9750155f15223b3153a0cc488
Your Name <you@example.com>
2025-12-10 11:43:57 +0530

    NumberAscii.java

---
+++ NumberAscii.java
@@ -0,0 +1,59 @@
+public class NumberAscii {
+    public static void main(String[] args){
+        int num=1;
+        if(num==0){
+            System.out.println("Ascii value of 0 "+48);
+        }else if(num==1){
+            System.out.println("Ascii value of 1 "+49);
+        }else if(num==2){
+            System.out.println("Ascii value of 2 "+50);
+        }else if(num==3){
+            System.out.println("Ascii value of 3 "+51);
+        }else if(num==4){
+            System.out.println("Ascii value of 4"+52);
+        }else if(num==5){
+            System.out.println("Ascii value of 5 "+53);
+        }else if(num==6){
+            System.out.println("Ascii value of 6 "+54);
+        }else if(num==7){
+            System.out.println("Ascii value of 7 "+55);
+        }else if(num==8){
+            System.out.println("Ascii value of 8 "+56);
+        }else if(num==9){
+            System.out.println("Ascii value of 9 "+57);
+        }
+
+        switch(num){
+            case 0:
+                System.out.println("Ascii value of 0 "+48);
+                break;
+            case 1:
+                System.out.println("Ascii value of 1 "+49);
+                break;
+            case 2:
+                System.out.println("Ascii value of 2 "+50);
+                break;
+            case 3:
+                System.out.println("Ascii value of 3 "+51);
+                break;
+            case 4:
+                System.out.println("Ascii value of 4 "+52);
+                break;
+            case 5:
+                System.out.println("Ascii value of 5 "+53);
+                break;
+            case 6:
+                System.out.println("Ascii value of 6 "+54);
+                break;
+            case 7:
+                System.out.println("Ascii value of 7 "+55);
+                break;
+            case 8:
+                System.out.println("Ascii value of 8 "+56);
+                break;
+            case 9:
+                System.out.println("Ascii value of 9 "+57);
+                break;
+        }
+    }
+}
